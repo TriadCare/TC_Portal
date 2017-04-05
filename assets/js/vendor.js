c1d502203bf0e80c2b4edc2ce5b8f0b9fa6289f0
Jack White <jwhite5672@gmail.com>
2017-04-05 15:45:24 -0400

    cleaned up vendor imports.

--- assets/js/vendor.js
+++ assets/js/vendor.js
@@ -1,17 +1,10 @@
-import '@blueprintjs/core';
 import '@blueprintjs/core/dist/blueprint.css';
-import '@blueprintjs/datetime';
 import '@blueprintjs/datetime/dist/blueprint-datetime.css';
-import '@blueprintjs/table';
 import '@blueprintjs/table/dist/table.css';
 import 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'react';
-import 'react-dom';
 import 'font-awesome/css/font-awesome.css';
-import 'react-chartist';
-import 'chartist/dist/chartist.min.css';
 // polyfills
 import 'whatwg-fetch';  // may need a promise polyfill for older browsers
 import 'core-js/client/shim';  // need this for Object.assign in IE
